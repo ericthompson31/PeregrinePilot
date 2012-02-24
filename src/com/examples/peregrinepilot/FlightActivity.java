df524fe7e1dd0e7225d6c97b56cfdb83a8e591a5
Eric Thompson <ericthompson31@gmail.com>
2012-02-24 15:45:22 -0600

    Added throttle bar to flight layout.  Updating percentage text and throttle status

--- src/com/examples/peregrinepilot/FlightActivity.java
+++ src/com/examples/peregrinepilot/FlightActivity.java
@@ -1,20 +1,62 @@
+
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.examples.peregrinepilot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
- * This activity will display an image and some information about Peregrine Engineering
- * @author Eric Thompson
+ * Demonstrates how to use a seek bar
  */
-public class FlightActivity extends Activity 
-{
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) 
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.flight);
-    }
+public class FlightActivity extends Activity implements
+		SeekBar.OnSeekBarChangeListener {
 
+	SeekBar mSeekBar;
+	TextView mProgressText;
+	TextView mTrackingText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.flight);
+
+		mSeekBar = (SeekBar) findViewById(R.id.seek);
+		mSeekBar.setOnSeekBarChangeListener(this);
+		mProgressText = (TextView) findViewById(R.id.progress);
+		mProgressText.setText("");
+		mTrackingText = (TextView) findViewById(R.id.tracking);
+		mTrackingText.setText("Ready");
+	}
+
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromTouch) {
+		mProgressText.setText(" " +progress+ "%");
+				//+ getString(R.string.seekbar_from_touch) + "=" + fromTouch);
+	}
+
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		mTrackingText.setText(getString(R.string.seekbar_tracking_on));
+	}
+
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		mTrackingText.setText(getString(R.string.seekbar_tracking_off));
+	}
 }
