c69f3ef6ea201d6c9adb8ce2396b61a0e09509fa
Eric Thompson <ericthompson31@gmail.com>
2012-02-25 19:06:12 -0600

    Improved throttle bar.  Added accelerometer output to the flight GUI (to be used for flight direction in the future)

--- src/com/examples/peregrinepilot/FlightActivity.java
+++ src/com/examples/peregrinepilot/FlightActivity.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (C) 2007 The Android Open Source Project
  *
@@ -17,7 +16,14 @@
 
 package com.examples.peregrinepilot;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -31,6 +37,11 @@ public class FlightActivity extends Activity implements
 	SeekBar mSeekBar;
 	TextView mProgressText;
 	TextView mTrackingText;
+	private TextView accText;
+	private SensorManager myManager;
+	private List<Sensor> sensors;
+	private Sensor accSensor;
+	private float oldX, oldY, oldZ = 0f;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +55,21 @@ public class FlightActivity extends Activity implements
 		mProgressText.setText("");
 		mTrackingText = (TextView) findViewById(R.id.tracking);
 		mTrackingText.setText("Ready");
+
+		accText = (TextView) findViewById(R.id.accText);
+
+		// Set Sensor + Manager
+		myManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		sensors = myManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+		if (sensors.size() > 0) {
+			accSensor = sensors.get(0);
+		}
 	}
 
 	public void onProgressChanged(SeekBar seekBar, int progress,
 			boolean fromTouch) {
-		mProgressText.setText(" " +progress+ "%");
-				//+ getString(R.string.seekbar_from_touch) + "=" + fromTouch);
+		mProgressText.setText(" " + progress + "%");
+		// + getString(R.string.seekbar_from_touch) + "=" + fromTouch);
 	}
 
 	public void onStartTrackingTouch(SeekBar seekBar) {
@@ -58,5 +78,40 @@ public class FlightActivity extends Activity implements
 
 	public void onStopTrackingTouch(SeekBar seekBar) {
 		mTrackingText.setText(getString(R.string.seekbar_tracking_off));
+	}
+
+	private void updateTV(float x, float y, float z)
+    {
+     float thisX = x - oldX * 10;
+     float thisY = y - oldY * 10;
+     float thisZ = z - oldZ * 10;
+     
+     accText.setText("X: " + Math.round(thisX) + ";  Y:" + Math.round(thisY) + ";  Z: " + Math.round(thisZ));
+     
+     oldX = x;
+     oldY = y;
+     oldZ = z;
+    }
+
+	private final SensorEventListener mySensorListener = new SensorEventListener() {
+		public void onSensorChanged(SensorEvent event) {
+			updateTV(event.values[0], event.values[1], event.values[2]);
+		}
+
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		}
+	};
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		myManager.registerListener(mySensorListener, accSensor,
+				SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	@Override
+	protected void onStop() {
+		myManager.unregisterListener(mySensorListener);
+		super.onStop();
 	}
 }
